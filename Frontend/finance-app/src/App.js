9870260503141d98fb1b423bff5bceba0c118933
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-10-12 23:18:37 +0530

    Updated

--- Frontend/finance-app/src/App.js
+++ Frontend/finance-app/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import api from "./api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [Transactions, setTransactions] = useState([]);
+  const [forData, setForData] = useState({
+    amount: "",
+    category: "",
+    description: "",
+    is_income: false,
+    date: "",
+  });
+
+  const fetchTransactions = async () => {
+    const response = await api.get("/transactions/");
+    setTransactions(response.data);
+  };
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  const handlerInputChange = (event) => {
+    const value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+    setForData({
+      ...FormData,
+      [event.target.name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    await api.post("/transactions/", formData);
+    fetchTransactions();
+  };
+};
 
 export default App;
